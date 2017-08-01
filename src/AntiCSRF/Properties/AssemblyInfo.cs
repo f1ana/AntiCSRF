161132dc5c14cbe50ff4e8d788115da0cd87f11d
Christopher Flanagan <chris@flana.com>
2017-07-31 18:20:05 -0700

    Add nuspec and fix name

--- src/AntiCSRF/Properties/AssemblyInfo.cs
+++ src/AntiCSRF/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("AntiCSRF")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Christophe Flanagan")]
+[assembly: AssemblyCompany("Christopher Flanagan")]
 [assembly: AssemblyProduct("AntiCSRF")]
 [assembly: AssemblyCopyright("MIT License")]
 [assembly: AssemblyTrademark("")]
